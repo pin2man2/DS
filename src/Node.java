e78a049424a05dff824cb332c5b703e167065b4f
pin2man2 <rk1991.tech@gmail.com>
2020-06-21 19:35:49 +0530

    first commit

---
+++ src/Node.java
@@ -0,0 +1,9 @@
+
+public class Node {
+	
+	int data;//value part or  data part
+	
+	Node next;//represent next node
+	
+
+}
---
+++ src/TestMain.java
@@ -0,0 +1,21 @@
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//deaclare the Object of Custom Linked List
+		
+		MyLinkedList list = new MyLinkedList();
+		
+		list.insert(3);
+		list.insert(14);
+		list.insert(2);
+		list.insert(9);
+		
+		//show all element 
+		
+		list.show();
+
+	}
+
+}
